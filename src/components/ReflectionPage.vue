95fc236566da5a523e740be1912ca3545c83c4a3
Philip Nous <philip12m2003@gmail.com>
2024-08-22 22:12:51 +0200

    final changes

--- src/components/ReflectionPage.vue
+++ src/components/ReflectionPage.vue
@@ -1,14 +1,46 @@
 <template>
-  <div style="display: flex; flex-direction: row;">
-    <div class="row-item">
-      <img src="../assets/x-factor-chart@2x.png" alt="photo of myself"
-           style="max-width: 50%; height: auto; border-radius: 50%"/>
-    </div>
-    <div class="row-item">
-      <h1 style="margin: 0 0 0 0;">Mijn vooruitgang</h1>
-      <p>
-        reflectie over mezelf
-      </p>
-    </div>
+  <div>
+    <img src="../assets/x-factor-chart@2x.png" alt="photo of myself"
+         style="max-width: 20%; height: auto; border-radius: 50%"/>
+  </div>
+  <h1 style="text-align: center">Mijn vooruitgang</h1>
+  <div style="padding: 0 8rem 0 8rem">
+    <p>
+      Ik ben blij met de persoon die ik ben geworden en hoe ik mezelf heb verbeterd in de laatste drie jaar.
+      Ik heb het meeste van mijn doelen bereikt. Ik ben vooral beter geworden in mijn vermogen om zelfstandig
+      keuzes te maken en in mijn manier van werken.
+      <br><br>
+      Door te werken aan verschillende projecten binnen mijn opleiding,  ben ik beter ben geworden in zelfstandig keuzes maken.
+      In het begin had ik hier wel moeite mee en stelde ik dit vaak uit. De belangrijkste dingen die ik heb geleerd, zijn om advies
+      te vragen aan anderen als dat mogelijk is en niet een halve keuze te proberen maken, waarbij ik dan aan de keuze blijf twijfelen
+      en achteraf weer verander, waardoor ik tijd verlies. Op dit moment kan ik zeggen dat dit niet probleem is voor mij.
+      <br><br>
+      Ook mijn algemene manier van werken is stukken efficiënter en productiever geworden in de laatste drie jaar.
+      Ik denk dat ik dit vooral te danken heb aan de POP-sessies die ik heb gevolgd. Zo heb ik nieuwe technieken
+      en principes geleerd die ik kan toepassen om mijn focus te behouden tijdens mijn werk en bepaalde valkuilen te vermijden.
+      Dit is dan ook iets wat ik graag wil aanhouden en wil blijven verbeteren waar mogelijk.
+      Daarom probeer ik me soms ook de vraag te stellen: “Ben ik nu op de juiste manier aan het werken of niet?” .
+      Recent probeer ik ook af en toe op zoek te gaan naar nieuwe tips die mijn manier van werken kunnen verbeteren.
+      <br><br>
+      Mijn teamwork en hoe extravert ik ben zijn ook wel verbeterd, maar wel wat minder dan ik gehoopt had. Tijdens het
+      IT-project in mijn derde jaar heb ik wel veel goede ervaring opgedaan en ben ik hierin verbeterd. Vooral op het vlak
+      van anderen aanspreken en participeren in de groep ben ik gegroeid. Aan het begin van het project werkte ik meer
+      zelfstandig, maar tijdens het project besefte ik dat ik soms beter hulp kan vragen aan een teamlid. Ik durf nu ook al
+      wat vaker tussenin een gesprek te komen om mijn eigen mening of advies te geven.
+      <br><br>
+      Ik vind dat mijn multidisciplinariteit, mijn (em)passie en hoe innovatief ik ben het meest zijn ontwikkeld.
+      Ik heb kennis opgedaan van verschillende (nieuwe) technologieën en domeinen die zowel buiten als binnen de informatica liggen.
+      Door de verschillende seminaries die ik heb gevolgd, heb ik ook al een sterk idee van welke nieuwe domeinen ik graag wil gaan
+      verkennen in mijn professionele leven.
+      <br><br>
+      Ik heb qua algemene samenwerking met anderen een goede basis opgebouwd, maar ik vind dat ik op
+      dit vlak mezelf nog wat extra kan ontwikkelen en misschien kan leren van anderen. Ik denk dat ik dit het best
+      kan verbeteren door mezelf in een omgeving te plaatsen waar ik veel met andere teamleden omga, in plaats van ergens
+      waar er veel zelfstandig gewerkt wordt. Dit komt doordat ik het gevoel had dat ik veel geleerd heb tijdens het IT-project,
+      waar ik constant met anderen omging. Dit is dan ook iets waar ik het liefst het meeste aandacht aan wil besteden in de toekomst,
+      zodat ik een nog betere professional kan worden.
+    </p>
   </div>
 </template>
+<script setup lang="ts">
+</script>
